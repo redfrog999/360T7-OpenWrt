10b0cff04dacc26961a112d650f52345dce722a7
David Tai <redfrog999@users.noreply.github.com>
2023-07-29 15:02:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,8 +22,8 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # rm -rf lua-maxminddb
 # git clone https://github.com/jerrykuku/lua-maxminddb
 
-# rm -rf luci-app-adguardhome
-# git clone https://github.com/AdguardTeam/AdGuardHome
+rm -rf luci-app-adguardhome
+git clone https://github.com/AdguardTeam/AdGuardHome
 
 # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
 rm -rf feeds/packages/net/v2ray-geodata
