59ea3e06a043641cc104a0bf0d485bfb94bd934e
David Tai <redfrog999@users.noreply.github.com>
2023-07-29 15:04:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,6 +27,7 @@ git clone https://github.com/AdguardTeam/AdGuardHome
 
 # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
 rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 feeds/packages/net/mosdns
 git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodata
 
