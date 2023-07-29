2087832663a2f5f24ad18dbdeec68f18e2bf3997
David Tai <redfrog999@users.noreply.github.com>
2023-07-29 09:39:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,3 +29,8 @@ git clone https://github.com/AdguardTeam/AdGuardHome
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+#安装Alist为最新版
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/luci-app-alist package/alist
