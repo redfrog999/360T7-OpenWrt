d5c7cfe6877d97f5888795c5b827a132e7e64c54
David Tai <redfrog999@users.noreply.github.com>
2023-09-09 19:48:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,5 +33,5 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 #安装Alist为最新版
 #rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 20.x packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 #git clone https://github.com/sbwml/luci-app-alist package/alist
