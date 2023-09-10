9a5e91cc12bb85250468beea599495134175f862
David Tai <redfrog999@users.noreply.github.com>
2023-09-10 17:30:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,8 +23,8 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # git clone https://github.com/jerrykuku/lua-maxminddb
 
 #安装xiaoroji版Passwall
-# rm -rf feeds/luci/applications/luci-app-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall luci/applications/luci-app-passwall
 
 # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
 rm -rf feeds/packages/net/v2ray-geodata
