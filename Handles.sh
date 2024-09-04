e6cc19e0770bc84af2548218ce3ae10be82901b7
David Tai <redfrog999@users.noreply.github.com>
2024-09-04 10:09:44 +0800

    Update Handles.sh

--- Handles.sh
+++ Handles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PKG_PATCH="$GITHUB_WORKSPACE/wrt/package/"
+
 #修改Tiny Filemanager汉化
 if [ -d *"tinyfilemanager"* ]; then
 	PO_FILE="./luci-app-tinyfilemanager/po/zh_Hans/tinyfilemanager.po"
@@ -11,81 +13,52 @@ fi
 
 #预置HomeProxy数据
 if [ -d *"homeproxy"* ]; then
-	HP_PATCH="../homeproxy/root/etc/homeproxy/resources"
+	HP_RULES="surge"
+	HP_PATCH="homeproxy/root/etc/homeproxy"
 
-	UPDATE_RESOURCES() {
-		local RES_TYPE=$1
-		local RES_REPO=$(echo "$2" | tr '[:upper:]' '[:lower:]')
-		local RES_BRANCH=$3
-		local RES_FILE=$4
-		local RES_EXT=${4##*.}
-		local RES_DEPTH=${5:-1}
+	chmod +x ./$HP_PATCH/scripts/*
+	rm -rf ./$HP_PATCH/resources/*
 
-		git clone -q --depth=$RES_DEPTH --single-branch --branch $RES_BRANCH "https://github.com/$RES_REPO.git" ./$RES_TYPE/
+	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULES/
+	cd ./$HP_RULES/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
 
-		cd ./$RES_TYPE/
+	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
+	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
+	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
+	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATCH/resources/
 
-		if [[ $RES_EXT == "txt" ]]; then
-			echo $(git log -1 --pretty=format:'%s' -- $RES_FILE | grep -o "[0-9]*") > "$RES_TYPE".ver
-			mv -f $RES_FILE "$RES_TYPE"."$RES_EXT"
-		elif [[ $RES_EXT == "zip" ]]; then
-			local REPO_ID=$(echo -n "$RES_REPO" | md5sum | cut -d ' ' -f 1)
-			local REPO_VER=$(git log -1 --pretty=format:'%s' | cut -d ' ' -f 1)
-			echo "{ \"$REPO_ID\": { \"repo\": \"$(echo $RES_REPO | sed 's/\//\\\//g')\", \"version\": \"$REPO_VER\" } }" > "$RES_TYPE".ver
-			curl -sfL -O "https://github.com/$RES_REPO/archive/$RES_FILE"
-			mv -f $RES_FILE $HP_PATCH/"${RES_REPO//\//_}"."$RES_EXT"
-		elif [[ $RES_EXT == "db" ]]; then
-			local RES_VER=$(git tag | tail -n 1)
-			echo $RES_VER > "$RES_TYPE".ver
-			curl -sfL -O "https://github.com/$RES_REPO/releases/download/$RES_VER/$RES_FILE"
-		fi
+	cd .. && rm -rf ./$HP_RULES/
 
-		cp -f "$RES_TYPE".* $HP_PATCH/
-		chmod +x $HP_PATCH/*
-
-		cd .. && rm -rf ./$RES_TYPE/
-	}
-
-	UPDATE_RESOURCES "china_ip4" "1715173329/IPCIDR-CHINA" "master" "ipv4.txt" "5"
-	UPDATE_RESOURCES "china_ip6" "1715173329/IPCIDR-CHINA" "master" "ipv6.txt" "5"
-	UPDATE_RESOURCES "gfw_list" "Loyalsoldier/v2ray-rules-dat" "release" "gfw.txt"
-	UPDATE_RESOURCES "china_list" "Loyalsoldier/v2ray-rules-dat" "release" "direct-list.txt"
-	#UPDATE_RESOURCES "geoip" "1715173329/sing-geoip" "master" "geoip.db"
-	#UPDATE_RESOURCES "geosite" "1715173329/sing-geosite" "master" "geosite.db"
-	#UPDATE_RESOURCES "clash_dashboard" "MetaCubeX/metacubexd" "gh-pages" "gh-pages.zip"
-
-	echo "homeproxy date has been updated!"
+	cd $PKG_PATCH && echo "homeproxy date has been updated!"
 fi
 
 #预置OpenClash内核和数据
-if [ -d *"OpenClash"* ]; then
-	CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/core_version"
-	CORE_TYPE=$(echo $WRT_TARGET | egrep -iq "64|86" && echo "amd64" || echo "arm64")
-	CORE_TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+if [ -d *"openclash"* ]; then
+	CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
+	CORE_TYPE=$(echo $WRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
+	CORE_TUN_VER=$(curl -sL $CORE_VER | sed -n "2{s/\r$//;p;q}")
 
-	#CORE_MASTER="https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-$CORE_TYPE.tar.gz"
-	CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/master/meta/clash-linux-$CORE_TYPE.tar.gz"
-	#CORE_TUN="https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-$CORE_TYPE-$CORE_TUN_VER.gz"
+	CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-$CORE_TYPE.tar.gz"
+	CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux-$CORE_TYPE.tar.gz"
+	CORE_TUN="https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux-$CORE_TYPE-$CORE_TUN_VER.gz"
 
 	GEO_MMDB="https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb"
-	#GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
-	#GEO_IP="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
-	GEO_META="https://github.com/MetaCubeX/meta-rules-dat/raw/release/geoip.metadb"
+	GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
+	GEO_IP="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
 
-	cd ./OpenClash/luci-app-openclash/root/etc/openclash/
+	cd ./luci-app-openclash/root/etc/openclash/
 
-	curl -sfL -o Country.mmdb $GEO_MMDB
-	#curl -sfL -o GeoSite.dat $GEO_SITE
-	#curl -sfL -o GeoIP.dat $GEO_IP
-	curl -sfL -o GeoIP.metadb $GEO_META
+	curl -sL -o Country.mmdb $GEO_MMDB && echo "Country.mmdb done!"
+	curl -sL -o GeoSite.dat $GEO_SITE && echo "GeoSite.dat done!"
+	curl -sL -o GeoIP.dat $GEO_IP && echo "GeoIP.dat done!"
 
 	mkdir ./core/ && cd ./core/
 
-	curl -sfL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta
-	#curl -sfL -o tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun
-	#curl -sfL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz
+	curl -sL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta && echo "meta done!"
+	curl -sL -o tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun && echo "tun done!"
+	curl -sL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && echo "dev done!"
 
-	chmod +x ./clash* && rm -rf ./*.gz
+	chmod +x ./* && rm -rf ./*.gz
 
-	echo "openclash date has been updated!"
+	cd $PKG_PATCH && echo "openclash date has been updated!"
 fi
