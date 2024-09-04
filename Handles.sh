d21130dc4775ca4daa7ad6ff27f33fc4c37ada69
David Tai <redfrog999@users.noreply.github.com>
2024-09-04 12:10:48 +0800

    Update Handles.sh

--- Handles.sh
+++ Handles.sh
@@ -2,27 +2,6 @@
 
 PKG_PATCH="$GITHUB_WORKSPACE/wrt/package/"
 
-#预置HomeProxy数据
-if [ -d *"homeproxy"* ]; then
-	HP_RULES="surge"
-	HP_PATCH="homeproxy/root/etc/homeproxy"
-
-	chmod +x ./$HP_PATCH/scripts/*
-	rm -rf ./$HP_PATCH/resources/*
-
-	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULES/
-	cd ./$HP_RULES/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
-
-	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
-	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
-	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
-	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATCH/resources/
-
-	cd .. && rm -rf ./$HP_RULES/
-
-	cd $PKG_PATCH && echo "homeproxy date has been updated!"
-fi
-
 #预置OpenClash内核和数据
 if [ -d *"openclash"* ]; then
 	CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
