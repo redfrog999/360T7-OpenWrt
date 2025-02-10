f6aa350f2edd822fc58a2ca3a7a25453150cc0d0
David Tai <redfrog999@users.noreply.github.com>
2025-02-10 21:16:22 +0800

    Update diy-part1-1.sh

--- diy-part1-1.sh
+++ diy-part1-1.sh
@@ -14,7 +14,7 @@
 #sed -i "/helloworld/d" "feeds.conf.default"
 #echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 #echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
