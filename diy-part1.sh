87094345df6c21730d61c6b740dbd3ce08a7d0cb
David Tai <redfrog999@users.noreply.github.com>
2025-10-10 08:08:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,6 +25,6 @@ echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.defa
 
 # 添加插件源码
 #sed -i '$a src-git kiddin9 https://github.com/redfrog999/kiddin9-openwrt-packages' feeds.conf.default
-sed -i '$a src-git-full kenzo https://github.com/redfrog999/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git-full kenzo https://github.com/redfrog999/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
