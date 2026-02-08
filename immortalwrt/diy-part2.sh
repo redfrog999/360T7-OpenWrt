50ab8ec9174474f48e9bdca16cba1c5b553121c4
David Tai <redfrog999@users.noreply.github.com>
2026-02-08 21:30:01 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -19,6 +19,22 @@ source ${GITHUB_WORKSPACE}/immortalwrt/function.sh
 # 默认IP由1.1修改为15.1
 sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
 
+# ---------------------------------------------------------
+# 物理獵人：Cargo 包管理器重定向
+# ---------------------------------------------------------
+echo "Configuring Cargo mirror..."
+mkdir -p ~/.cargo
+cat <<EOF > ~/.cargo/config
+[source.crates-io]
+replace-with = 'ustc'
+
+[source.ustc]
+registry = "git://mirrors.ustc.edu.cn/crates.io-index"
+EOF
+
+# 如果有自定義主題或特定插件，在這裡繼續執行你的原代碼
+# 例如：git clone ... package/lean/luci-theme-argon
+
 # 最大连接数修改为65535
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
