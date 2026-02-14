1f12286e6dc18ab04695d5a333e3053525e643ab
David Tai <redfrog999@users.noreply.github.com>
2026-02-14 17:59:58 +0800

    Create diy-part1-baili.sh

---
+++ immortalwrt/diy-part1-baili.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+echo "开始 DIY1 配置……"
+echo "========================="
+
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '1i src-git kenzo https://github.com/redfrog999/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' feeds.conf.default
+# sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+
+# rm -rf target/linux/ramips
+# svn export https://github.com/padavanonly/immortalwrt/trunk/target/linux/ramips target/linux/ramips
+
+# 在 diy-part1.sh 中加入
+# sed -i 's/src-git packages https:\/\/github.com\/immortalwrt\/packages.git;master/src-git packages https:\/\/github.com\/immortalwrt\/packages.git/g' feeds.conf.default
+
+# 修改系统版本（界面显示）
+VERSION=${GITHUB_WORKSPACE}/immortalwrt/version
+VERSION_TEXT=$(head -n 1 ${VERSION} | tr -d ' \r\n')
+if [ -n "$VERSION_TEXT" ]; then
+	sed -i "/^VERSION_NUMBER:=.*SNAPSHOT/s/SNAPSHOT/${VERSION_TEXT}/" include/version.mk
+fi
+echo "========================="
+echo " DIY1 配置完成……"
