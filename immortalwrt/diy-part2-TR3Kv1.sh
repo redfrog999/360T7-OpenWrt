e643a804dc697b3631f89adbf9d3aab671e4ab73
David Tai <redfrog999@users.noreply.github.com>
2026-02-16 14:09:39 +0800

    Update diy-part2-TR3Kv1.sh

--- immortalwrt/diy-part2-TR3Kv1.sh
+++ immortalwrt/diy-part2-TR3Kv1.sh
@@ -7,28 +7,6 @@ echo "========================="
 # 修改默认IP
 sed -i 's/192.168.6.1/192.168.15.1/g' package/base-files/files/bin/config_generate
 
-# 解除rustc依赖，但是扫描时精准避开“生命线”
-# 1.使用 -vE 逻辑，强制让 find 跳过这四个核心包
-find feeds/ -name Makefile -exec grep -l "DEPENDS:=.*rust" {} + | grep -vE "smartdns|ruby|dnsmasq-full" | xargs rm -rf
-
-# --- [依赖名对齐：解决图 7 报错的关键] ---
-# 2. 强制将 OpenClash 依赖的 dnsmasq-full-full 缩减为 dnsmasq-full
-# 这一步是为了干掉那个多余的 "-full" 后缀
-find package/ -name Makefile -exec sed -i 's/dnsmasq-full-full/dnsmasq-full/g' {} +
-find feeds/ -name Makefile -exec sed -i 's/dnsmasq-full-full/dnsmasq-full/g' {} +
-
-# 3. 菜单逻辑二次加固
-# 在脚本末尾再次显式声明，确保这些包被选中且不会被后续逻辑剔除
-echo "CONFIG_PACKAGE_dnsmasq-full=y" >> .config
-echo "CONFIG_PACKAGE_smartdns=y" >> .config
-echo "CONFIG_PACKAGE_ruby=y" >> .config
-echo "CONFIG_PACKAGE_ruby-yaml=y" >> .config
-echo "CONFIG_PACKAGE_dnsmasq=n" >> .config
-
-# 4. 彻底屏蔽 Rust 相关的配置条目
-sed -i 's/CONFIG_PACKAGE_rust=y/# CONFIG_PACKAGE_rust is not set/g' .config
-sed -i 's/CONFIG_PACKAGE_librsvg=y/# CONFIG_PACKAGE_librsvg is not set/g' .config
-
 # 彻底清理 PassWall、老旧 OpenClash 和残留核心库 (防止逻辑冲突)
 rm -rf feeds/packages/net/{xray*,v2ray*,sing-box,hysteria*,shadowsocks*,trojan*,clash*}
 rm -rf feeds/luci/applications/luci-app-passwall
@@ -68,6 +46,12 @@ git clone https://github.com/nikkinikki-org/OpenWrt-nikki package/luci-app-nikki
 find ./ -name "luci-app-openclash" -type d -exec rm -rf {} +
 git clone --depth 1 -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 sed -i 's/dnsmasq/dnsmasq-full/g' package/luci-app-openclash/luci-app-openclash/Makefile
+
+# 物理注入 Rustc 1.90.0 (核心规避手段)
+# 1. 物理注入 Rustc 源码 (解决下载失败) ---
+mkdir -p dl
+RUST_URL="https://github.com/redfrog999/JDCloud-AX6000/releases/download/rustc_1.9.0/rustc-1.90.0-src.tar.xz"
+wget -qO dl/rustc-1.90.0-src.tar.xz "$RUST_URL"
 
 # --- 3. 硬件性能加速与指令集对齐 (SafeXcel & A53) ---
 
